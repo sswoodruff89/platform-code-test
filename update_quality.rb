4066e7d506b1cfa4a1f6cded7da6cd7975fa99bd
sean <sswoodruff89@gmail.com>
2020-04-15 16:31:20 -0400

    added Blue Star and passed specs, commented out before {pendin} on line 180 in spec

--- update_quality.rb
+++ update_quality.rb
@@ -2,45 +2,50 @@ require 'award'
 
 def update_quality(awards)
   awards.each do |award| 
-         
-    # if (award.quality != 0)
-      if award.name != 'Blue First' && award.name != 'Blue Compare'
+      
+    if award.name != 'Blue First' &&
+        award.name != 'Blue Compare' && 
+          award.name != 'Blue Star'
 
-        if award.name != 'Blue Distinction Plus'
-            award.quality = [award.quality - 1, 0].max
-        end
+      if award.name != 'Blue Distinction Plus'
+          award.quality = [award.quality - 1, 0].max
+      end
 
+    else
+      
+      if award.name == 'Blue Compare'
+        award.quality += (award.expires_in <= 5) ? 2 
+        : (award.expires_in <= 10) ? 1 
+          : 0
+      end
+    
+      if award.name == 'Blue Star'
+        award.quality = [award.quality - 2, 0].max
       else
-        
-        if award.name == 'Blue Compare'
-          award.quality += (award.expires_in <= 5) ? 2 
-            : (award.expires_in <= 10) ? 1 
-              : 0
-        end
         award.quality = [50, award.quality + 1].min
-
       end
 
+    end
 
-      award.expires_in -= (award.name != 'Blue Distinction Plus') ? 1 : 0
+    award.expires_in -= (award.name != 'Blue Distinction Plus') ? 1 : 0
 
+    if award.expires_in < 0
 
-      if award.expires_in < 0
+      if award.name != 'Blue First' &&
+        award.name != 'Blue Compare' && 
+          award.name != 'Blue Distinction Plus' &&
+            award.name != 'Blue Star'
 
-        if award.name != 'Blue First' &&
-          award.name != 'Blue Compare' && 
-            award.name != 'Blue Distinction Plus'
+        award.quality = [award.quality - 1, 0].max       
 
-          award.quality = [award.quality - 1, 0].max       
-
-        elsif award.name == 'Blue Compare'
-            award.quality = award.quality - award.quality
-        elsif award.name == 'Blue First'
-          award.quality = [50, award.quality + 1].min
-        end
+      elsif award.name == 'Blue Compare'
+        award.quality = award.quality - award.quality
+      elsif award.name == 'Blue First'
+        award.quality = [50, award.quality + 1].min
+      elsif award.name == 'Blue Star'
+        award.quality = [award.quality - 2, 0].max
       end
-
-    # end
+    end
 
   end
 end
@@ -60,13 +65,6 @@ end
 #       end
 #     else
 #       award.quality = [50, award.quality + 1].min
-      
-
-
-
-
-
-
 #       if award.quality < 50
 #         award.quality += 1
 #         if award.name == 'Blue Compare'
@@ -82,13 +80,10 @@ end
 #           end
 #         end
 #       end
-
 #     end
-
 #     if award.name != 'Blue Distinction Plus'
 #       award.expires_in -= 1
 #     end
-
 #     if award.expires_in < 0
 #       if award.name != 'Blue First'
 #         if award.name != 'Blue Compare'
@@ -106,7 +101,5 @@ end
 #         end
 #       end
 #     end
-
-
 #   end
 # end
